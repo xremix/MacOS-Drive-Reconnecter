bd455fdab26bde81411790b9cc513a47fade5561
xremix <tonidhoffmann@gmail.com>
2018-09-19 13:42:00 +0200

    init project

---
+++ MacOS-Drive-Reconnecter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacOS-Drive-Reconnecter
+//
+//  Created by Toni Hoffmann on 19.09.18.
+//  Copyright © 2018 Toni Hoffmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacOS-Drive-Reconnecter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOS-Drive-Reconnecter
+//
+//  Created by Toni Hoffmann on 19.09.18.
+//  Copyright © 2018 Toni Hoffmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
